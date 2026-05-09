75de7073d77812a6035f814f9795045a2f88b4ab
tundut <ducnguyenphan05@gmail.com>
2026-05-09 16:56:13 +0700

    Fix: fix get token data

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -3,7 +3,7 @@ const provider = new ethers.JsonRpcProvider(
 );
 
 // const API = 'http://localhost:3000/api';
-const API = 'https://crypto-wallet-1-ilnn.onrender.com';
+const API = 'https://crypto-wallet-1-ilnn.onrender.com/api';
 let activeWallet = null;
 let activeWalletIndex = 0;
 let walletBalance = 0;
